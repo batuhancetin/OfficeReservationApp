5941bec1240e6d999f524a5a6d8ad887aee3ba53
Batuhan Cetin <77202451+batuhancetin@users.noreply.github.com>
2023-02-28 23:12:19 +0300

    Project finalized

--- src/utils/caching.ts
+++ src/utils/caching.ts
@@ -2,7 +2,10 @@ import * as redis from 'redis';
 import logger from './logger';
 
 const redisClient = redis.createClient({
-    url: 'redis://redis:6380',
+    url: 'redis://redis:6379',
+    socket: {
+        connectTimeout: 50000
+    }
 });
 
 const start = async () => {
